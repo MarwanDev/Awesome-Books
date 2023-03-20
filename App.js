7fb56bde5db933291bccb2fd46f8efe4b6e646f3
MarwanDev <marwaneg@aucegypt.edu>
2023-03-20 10:35:49 +0200

    Add book list

---
+++ App.js
@@ -0,0 +1,25 @@
+const booksArray = [
+  {
+    id: 1,
+    title: 'Lorem Ipsom',
+    author: 'Bala7',
+  },
+  {
+    id: 2,
+    title: 'Lorem Ipsom',
+    author: 'Bala7',
+  },
+];
+
+const bookList = document.getElementById('books-list');
+const booksDisplay = (id, title, author) => `    <h2>${title}</h2>
+<h3>${author}</h3>
+<button type="button" class="book-btn" id="book-btn-${id}">Remove</button>
+<hr>`;
+
+booksArray.forEach((book) => {
+  const htmlToAdd = booksDisplay(book.id,
+    book.title,
+    book.author);
+  bookList.insertAdjacentHTML('afterbegin', htmlToAdd);
+});
