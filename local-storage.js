c29b7b56a07947f3b232ea933e7c28e4cebf1912
MarwanDev <marwaneg@aucegypt.edu>
2023-03-20 11:44:44 +0200

    Fix linter errors

--- local-storage.js
+++ local-storage.js
@@ -1,20 +1,20 @@
-const localStorageInputs = document.querySelectorAll(".local-storage");
+const localStorageInputs = document.querySelectorAll('.local-storage');
 const localStorage = {
-    bookTitle: "",
-    bookAuthor: "",
+  bookTitle: '',
+  bookAuthor: '',
 };
 
 localStorageInputs.forEach((input) => {
-    input.addEventListener('input', () => {
-      localStorage[input.bookTitle] = input.value;
-      localStorage[input.bookAuthor] = input.value;
-      localBrowserStorage.setItem('information', JSON.stringify(localStorage));
-    });
-})
+  input.addEventListener('input', () => {
+    localStorage[input.bookTitle] = input.value;
+    localStorage[input.bookAuthor] = input.value;
+    localBrowserStorage.setItem('information', JSON.stringify(localStorage));
+  });
+});
 
 const StoredBrowserData = JSON.parse(localStorage.getItem('information'));
 if (StoredBrowserData) {
-    localStorageInputs.forEach((element) => {
-      element.value = StoredBrowserData[element.bookTitle];
-    });
-  }
+  localStorageInputs.forEach((element) => {
+    element.value = StoredBrowserData[element.bookTitle];
+  });
+}
