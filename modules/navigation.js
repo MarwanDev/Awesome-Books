375bded11cc824c558c89bf7479b48087dfa8d25
MarwanDev <marwaneg@aucegypt.edu>
2023-03-27 15:29:12 +0200

    Add ES6 features and luxon date

---
+++ modules/navigation.js
@@ -0,0 +1,7 @@
+export const booksList = document.getElementById('books-list-section');
+export const addBooks = document.getElementById('add-book-section');
+export const contactUs = document.getElementById('contact-us');
+
+export const listLink = document.getElementById('list-link');
+export const addNewLink = document.getElementById('add-new-link');
+export const contactLink = document.getElementById('contact-link');
